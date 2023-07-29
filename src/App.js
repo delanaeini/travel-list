ffe815bb58684be924b49cd6e6e86c5d50a43945
Dela Naeini <delaram.naeini@gmail.com>
2023-07-29 09:51:01 -0700

    form working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+// const testItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: true },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Chargers", quantity: 3, packed: false },
+// ];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    //will be used by the form component
+    setItems((items) => [...items, item]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState(""); //for form input state
+  const [quantity, setQuantity] = useState(1); //for form select
+
+  function handleSubmit(e) {
+    //e is the event object
+    e.preventDefault(); //to not reload the form after submit
+
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {/* <option value="1">1</option>       Wrong value!!*/}
+        {/* <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+        <option value={4}>4</option>       Too long! There's a better way */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)} //e.target is the entire input element(type, placeholder, etc.)
+      />
+      <button>ADD</button>{" "}
+      {/*If we had put the event handler inside the button, 
+      we couldn't submit the form by pressing return key while inside a form input*/}
+    </form>
+  );
+}
+
+function PackingList({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map(
+          //(i) => (<li>i.description</li>) Wrong!!
+          (item) => (
+            <Item item={item} key={item.id} />
+          ) //object => <Component prop={object} />
+        )}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={{ textDecoration: item.packed ? "line-through" : "none" }}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on you list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
